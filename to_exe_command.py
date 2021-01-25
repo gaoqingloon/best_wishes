4c3e3425b2ce4aaa614298d4c97e3bf64fcb88a2
gaoqinglong <gordon_ml@163.com>
2021-01-25 22:41:28 +0800

    version 1.1.2

---
+++ to_exe_command.py
@@ -0,0 +1,15 @@
+# import PyInstaller.__main__
+# import os
+#
+# name = "best_wishes"
+# script_name = "best_wishes.py"
+#
+# PyInstaller.__main__.run([
+#     '--name=%s' % name,
+#     '--onefile',
+#     '--windowed',
+#     '--icon=%s' % os.path.join('D:/myStudyProject/py_project/', 'icon.ico'),
+#     os.path.join('D:/myStudyProject/py_project/', script_name),
+# ])
+
+# pyinstaller -F best_wishes.py --icon=D:/myStudyProject/py_project/icon2.ico
