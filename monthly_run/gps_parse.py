3f7afc0130f3f8227fd5c98f9ee0ffc557232157
gaoqinglong <gordon_ml@163.com>
2021-02-04 09:04:22 +0800

    release 2.1.1 update gps parse

--- monthly_run/gps_parse.py
+++ monthly_run/gps_parse.py
@@ -1,22 +1,24 @@
 # coding: utf-8
 
-# ### 1 读文件并完成解析
 import gpxpy.parser as parser
 
 file_name = "跑步20210131154035.gpx"
 
-
-with open('./' + file_name, 'r', encoding="utf-8") as fr:
-    with open("./r_" + file_name, 'w', encoding="utf-8") as fw:
-        for line in fr:
-            line = line.replace("<metadata>", "").replace("</metadata>", "").replace("type", "desc")
-            fw.write(line)
+# with open('./' + file_name, 'r', encoding="utf-8") as fr:
+#     with open("./r_" + file_name, 'w', encoding="utf-8") as fw:
+#         for line in fr:
+#             line = line \
+#                 .replace("<metadata>", "") \
+#                 .replace("</metadata>", "") \
+#                 .replace("<extensions>", "") \
+#                 .replace("</extensions>", "") \
+#                 .replace("type", "desc").replace("heartrate", "magvar").replace("distance", "geoidheight")
+#             fw.write(line)
 
 gpx_file = open("./r_" + file_name, 'r', encoding="utf-8")
 gpx_parser = parser.GPXParser(gpx_file)
 gpx = gpx_parser.parse()  # 文件解析
 gpx_file.close()
-
 
 """
 <metadata>
@@ -24,31 +26,32 @@ gpx_file.close()
 type -> desc
 
 """
-# ### 2 读文件并完成解析
 print('======================')
 print(gpx.time)
-print('======================')
 
-# ### 3 打印解析的轨迹数据
+# 打印解析的轨迹数据
 for track in gpx.tracks:
     i = 0
     print(track.name)
     print(track.description)
+    print('======================')
 
     for segment in track.segments:
         for point in segment.points:
-            print(str(point.latitude) + ", " +
-                  str(point.longitude) + ", " +
-                  str(point.time) + ", " + str(point.elevation))
+            time = point.time
+            latitude = point.latitude
+            longitude = point.longitude
+            heart_rate = point.magnetic_variation
+            distance = point.geoid_height
+            elevation = point.elevation
+            print(str(time) + "," +
+                  str(latitude) + "," +
+                  str(longitude) + "," +
+                  str(heart_rate) + "," +
+                  str(distance) + "," +
+                  str(elevation))
 
-            for ele in point.extensions:
-                print(ele)
-            i += 1
-            if i == 3:
-                break
-
-# ### 6 其他方法
-# print("*" * 50)
-# print('GPX:', gpx.to_xml())
-
+            # i += 1
+            # if i == 300:
+            #     break
 
